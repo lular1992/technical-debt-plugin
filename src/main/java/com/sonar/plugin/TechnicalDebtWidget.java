6c259ec60a9f7fb3bf3c4edae820174b1c825567
Ana María <lular1992@gmail.com>
2014-02-06 13:53:56 +0100

    Compiles

--- src/main/java/com/sonar/plugin/TechnicalDebtWidget.java
+++ src/main/java/com/sonar/plugin/TechnicalDebtWidget.java
@@ -11,29 +11,4 @@ import org.sonar.api.SonarPlugin;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * This class is the entry point for all extensions
- */
-@Properties({
-  @Property(
-    key = ExamplePlugin.MY_PROPERTY,
-    name = "Plugin Property",
-    description = "A property for the plugin",
-    defaultValue = "Hello World!")})
-public final class ExamplePlugin extends SonarPlugin {
-
-  public static final String MY_PROPERTY = "sonar.example.myproperty";
-
-  // This is where you're going to declare all your Sonar extensions
-  public List getExtensions() {
-    return Arrays.asList(
-        // Definitions
-        ExampleMetrics.class,
-
-        // Batch
-        ExampleSensor.class, RandomDecorator.class,
-
-        // UI
-        ExampleFooter.class, ExampleRubyWidget.class);
-  }
-}
+public class TechnicalDebtWidget {}
